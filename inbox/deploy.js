0e417a63372367eced76adfc975e9487431bdc6e
Luiz Fernando Veríssimo <62751844+luizfverissimo@users.noreply.github.com>
2021-12-05 11:11:07 -0300

    End of section 2

--- inbox/deploy.js
+++ inbox/deploy.js
@@ -1,1 +1,30 @@
-// deploy code will go here
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+require('dotenv').config();
+
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+  process.env.MNEUMONIC,
+  process.env.RINKEBY_ENDPOINT
+);
+
+const web3 = new Web3(provider);
+
+(async () => {
+  const accounts = await web3.eth.getAccounts();
+  console.log('💳 Attempting to deploy from account: ', accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({
+      data: bytecode,
+      arguments: ['Olá Mundão!']
+    })
+    .send({
+      gas: '1000000',
+      from: accounts[0]
+    });
+
+  console.log('📬 Contract deployed to: ', result.options.address)
+  provider.engine.stop()
+})();
